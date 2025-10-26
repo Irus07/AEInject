72be49608f82e00f29339e844f3396f1f4d438a3
Irus <cc>
2025-10-26 17:36:38 +0300

    The generator has been changed : moving to a newer version of the generator - IIncrementalGenerator

---
+++ src/DIFactoryGenerator/CodeBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace DIFactoryGenerator
+{
+	internal static class CodeBuilder
+	{
+		const string NAME_POSTFIX = DIFactoryGenerator.NAME_POSTFIX;
+
+
+		internal static void GenerateSourceCode(SourceProductionContext context, ISymbol symbol)
+		{
+
+			//Debugger.Launch();
+
+
+			string code = $@"
+				namespace DIFactoryGenerator.Factories 
+							{{
+								public static class {symbol.Name + "_IncrementalFactory"} 
+								{{
+									{GetConstructors(symbol)}
+								}}
+							}}";
+
+			context.AddSource(symbol.Name + NAME_POSTFIX + ".g", code);
+
+		}
+		private static string GetConstructors(ISymbol symbol)
+		{
+			if (!(symbol is INamedTypeSymbol typeSymbol))
+			{
+				return "";
+			}
+
+			string className = typeSymbol.Name;
+			var constructors = typeSymbol.Constructors;
+
+			StringBuilder sb = new StringBuilder();
+
+			foreach (var constructor in constructors)
+			{
+				//sb.Append($"public static {symbol.Name}  Create(");
+				sb.Append($"public static void  Create(");
+				int leng = constructor.Parameters.Length, counter = 0;
+				foreach (var parameter in constructor.Parameters)
+				{
+					string paramType = parameter.Type.ToDisplayString();
+					string paramName = parameter.Name;
+					sb.Append(paramType + $" {paramName}");
+					if (counter < leng - 1)
+					{
+						sb.Append(", ");
+					}
+					counter++;
+
+				}
+
+
+				//sb.Remove(sb.Length - 1, 1);
+				sb.Append(")");
+				sb.AppendLine("{}");
+			}
+
+			return sb.ToString();
+		}
+	}
+}
--- src/DIFactoryGenerator/DIFactoryGenerator.cs
+++ src/DIFactoryGenerator/DIFactoryGenerator.cs
@@ -1,129 +1,51 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Reflection.Metadata;
 
 namespace DIFactoryGenerator
 {
 	[Generator]
-	public class DIFactoryGenerator : ISourceGenerator
+	public class DIFactoryGenerator : IIncrementalGenerator
 	{
-		public void Execute(GeneratorExecutionContext context)
+		public const string ATTR_NAME = "AEInject.Lib.Attribute.DIFactoryAttribute";
+		public const string NAME_POSTFIX = "_IncrementalFactory";
+
+
+		public void Initialize(IncrementalGeneratorInitializationContext context)
 		{
-			if (!(context.SyntaxReceiver is SyntaxAnalyzer analyzer))
-				return;
 
-			foreach(ClassDeclarationSyntax item in analyzer.PossibleClasses)
+			var classProvider = context.SyntaxProvider
+				.CreateSyntaxProvider(
+					(s, _) => (s is ClassDeclarationSyntax),
+					(ctx, _) =>
+					{
+						var classSyntax = (ClassDeclarationSyntax)ctx.Node;
+						return ctx.SemanticModel.GetDeclaredSymbol(classSyntax);
+					})
+				.Where(rez => !(rez is null));
+
+
+
+			var filteredClasses =
+				from x in classProvider
+				where x.GetAttributes()
+					.Any(atr => atr.AttributeClass.ToDisplayString() == ATTR_NAME)
+				select x;
+
+
+			context.RegisterSourceOutput(filteredClasses, (productionContext, classSymbol) =>
 			{
-				ProcessClass(context, item);
+				CodeBuilder.GenerateSourceCode(productionContext, classSymbol);
+			});
 
-			}
-
-			string code = $@"
-			namespace DIFactoryGenerator.Factories 
-			{{
-				public static class qwert
-				{{
-		
-				}}
-			}}";
-
-			context.AddSource("qw", code);
-		}
-
-		private void ProcessClass(GeneratorExecutionContext context , ClassDeclarationSyntax syntax )
-		{
-			var model = context.Compilation.GetSemanticModel(syntax.SyntaxTree);
-			var symbol = model.GetDeclaredSymbol(syntax) as INamedTypeSymbol;
-
-			if(symbol is null)
-				return;
-
-			if (HasFactory(symbol))
-			{
-				GenerateFactory(context, symbol);
-			}
 
 		}
 
-		private bool HasFactory(INamedTypeSymbol symbol)
-		{
-			foreach(var item in symbol.GetAttributes())
-			{
-				var name = item.AttributeClass.ToDisplayString();
-				if(name == "AEInject.Lib.Attribute.DIFactoryAttribute")
-				{
-					return true;
-				}
-			}
-			return false;
-		}
-
-		private DIFactoryInfo GetFactoryInfo(INamedTypeSymbol symbol)
-		{
-			var factoryAtrr = symbol.GetAttributes()
-				.FirstOrDefault(c =>
-				c.AttributeClass.ToDisplayString() == "AEInject.Lib.Attribute.DIFactoryAttribute");
-
-			if (factoryAtrr == null)
-				return null ;
-
-			var info = new DIFactoryInfo();
-
-			if(factoryAtrr.ConstructorArguments.Length > 0)
-			{
-				foreach (var item in factoryAtrr.ConstructorArguments)
-				{
-					if(item.Kind == TypedConstantKind.Primitive && item.Value is string value )
-						info.Name = value;
-					
-				}
-			}
-
-			
-			foreach(var item in factoryAtrr.NamedArguments)
-			{
-				if(item.Key == "FactoryName" && item.Value.Value is string value)
-				{
-					info.Name = value;
-				}
-			}
-
-			if (string.IsNullOrEmpty(info.Name))
-			{
-				info.Name = $"{symbol.Name}_DIFactory";
-			}
-
-			return info;
-
-		}
-
-		
 
 
-
-		private void GenerateFactory(GeneratorExecutionContext context , INamedTypeSymbol symbol)
-		{
-			DIFactoryInfo dIFactoryInfo = GetFactoryInfo(symbol);
-
-
-			string code = $@"
-			namespace DIFactoryGenerator.Factories 
-			{{
-				public static class {dIFactoryInfo.Name} 
-				{{
-		
-				}}
-			}}";
-
-			context.AddSource(dIFactoryInfo.Name, code);
-		}
-
-
-		public void Initialize(GeneratorInitializationContext context)
-		{
-			context.RegisterForSyntaxNotifications(() => new SyntaxAnalyzer());
-		}
 	}
 }
