8b8ab3b476502e3397bcaa763460e8237a0d5786
Irus <cc>
2025-11-23 16:08:01 +0300

    rollback to the start of vibcoding

---
+++ src/AEinject/Lib/DI/Locator.cs
@@ -0,0 +1,117 @@
+﻿using AEinject.Lib.Attribute;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace AEinject.Lib
+{
+	public static class FactoryLocator
+	{
+		public static readonly ConcurrentDictionary<Type, IEnumerable<MethodInfo>> _factories;
+		private static bool _isInit = false;
+
+		static FactoryLocator()
+		{
+			_factories = new ConcurrentDictionary<Type, IEnumerable<MethodInfo>>();
+		}
+		public static void Init()
+		{
+			if (!_isInit)
+			{
+				TriggerAttributes();
+				_isInit = true;
+			}
+			return;
+		}
+
+		public static void Register(Type factoryType, Type targetType)
+		{
+			if (_factories.ContainsKey(factoryType))
+			{
+				return;
+			}
+
+			var targetMethods =
+				from method in targetType.GetMethods()
+				where method.Name == "Create"
+				select method;
+
+			_factories.TryAdd(targetType, targetMethods);
+
+		}
+		internal static T1 CreateInstance<T1>(object[] @params = null)
+		{
+
+			var targetMethod = _factories[typeof(T1)].FirstOrDefault((method) => ParametersMatch(method.GetParameters(), @params.Select((c) => c.GetType()).ToArray()));
+
+			if (targetMethod.Invoke(null, @params) is T1 targetObject)
+			{
+				return targetObject;
+			}
+
+			throw new ArgumentException($"the factory method for creating a {typeof(T1).ToString()} type object with @params parameters was not found", nameof(@params));
+		}
+
+		private static bool ParametersMatch(ParameterInfo[] parameters, Type[] argumentTypes)
+		{
+			if (parameters.Length != argumentTypes.Length)
+				return false;
+
+			for (int i = 0; i < parameters.Length; i++)
+			{
+				if (!parameters[i].ParameterType.IsAssignableFrom(argumentTypes[i]))
+					return false;
+			}
+
+			return true;
+		}
+
+		private static void TriggerAttributes()
+		{
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			Type targetAttribute = typeof(LocatorFactoryAttribute);
+
+			var targetAssemblies =
+				from assembly in assemblies
+				let assemblyName = assembly.GetName().Name
+				where !assemblyName.StartsWith("System.") &&
+					  !assemblyName.StartsWith("Microsoft.") &&
+					  !assemblyName.StartsWith("Windows.") &&
+					  !assemblyName.StartsWith("mscorlib") &&
+					  !assemblyName.StartsWith("netstandard") &&
+					  !assemblyName.Equals("System") &&
+					  !assembly.IsDynamic
+				select assembly;
+
+
+
+			Parallel.ForEach(targetAssemblies, (assembly) =>
+			{
+				try
+				{
+					foreach (var type in assembly.GetTypes())
+					{
+						try
+						{
+							if (type.IsDefined(targetAttribute, false))
+							{
+								var attributes = type.GetCustomAttributes(targetAttribute, false);
+							}
+						}
+						catch (Exception ex) when (ex is TypeLoadException || ex is FileNotFoundException || ex is BadImageFormatException)
+						{
+						}
+
+					}
+				}
+				catch (Exception ex) when (ex is FileNotFoundException || ex is BadImageFormatException || ex is ReflectionTypeLoadException)
+				{
+
+				}
+
+			});
+
+		}
+	}
+}
--- src/DIFactoryGenerator/Builders/MethodBuilder.cs
+++ src/DIFactoryGenerator/Builders/MethodBuilder.cs
@@ -6,104 +6,126 @@ using System.Text;
 
 namespace DIFactoryGenerator.Builders
 {
-    class MethodBuilder
-    {
-        private readonly string _accessModifier;
-        private readonly string _name;
-        private readonly string _type;
-        private readonly bool _isStatic;
-        private readonly List<(string type, string paramName)> _params;
-        private StringBuilder _body = new StringBuilder();
+	class MethodBuilder
+	{
+		private readonly string _accessModifier;
+		private readonly string _name;
+		private readonly string _type;
+		private readonly bool _isStatic;
+		private readonly List<(string type, string paramName)> _params;
+		private StringBuilder _body = new StringBuilder();
 
-        public StringBuilder Body
-        {
-            get => _body;
-            set => _body = value;
-        }
+		public StringBuilder Body
+		{
+			get => _body;
+			set => _body = value;
+		}
 
-        public MethodBuilder(string accessModifier, bool isStatic, string returnType, string name, IEnumerable<(string type, string paramName)> @params = null)
-        {
-            _accessModifier = accessModifier;
-            _isStatic = isStatic;
-            _type = returnType;
-            _name = name;
-            _params = @params.ToList();
-
-        }
-        public MethodBuilder(string accessModifier, bool isStatic, string returnType, string name, params (string type, string paramName)[] @params)
-        {
-            _accessModifier = accessModifier;
-            _isStatic = isStatic;
-            _type = returnType;
-            _name = name;
-            _params = @params.ToList();
-        }
-        public MethodBuilder(string accessModifier, bool isStatic, string returnType, string name, IMethodSymbol @params)
-        {
-            _accessModifier = accessModifier;
-            _isStatic = isStatic;
-            _type = returnType;
-            _name = name;
-            _params = new List<(string type, string paramName)>();
-
-            string paramType, paramName;
-
-            foreach (var symbol in @params.Parameters)
-            {
-                paramType = symbol.Type.ToDisplayString();
-                paramName = symbol.Name;
-
-                _params.Add((paramType, paramName));
-            }
-
-        }
-        public MethodBuilder(string returnType, string name, IEnumerable<(string type, string paramName)> @params = null, string accessModifier = "private", bool isStatic = false)
-        {
-            _accessModifier = accessModifier;
-            _isStatic = isStatic;
-            _type = returnType;
-            _name = name;
-            _params = @params.ToList();
-        }
-
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-
-            string @static = _isStatic ? "static" : string.Empty;
-
-            sb.AppendLine($"{_accessModifier} {@static} {_type} {_name} ({ParamsToString()})");
-            sb.AppendLine("{");
-
-            foreach (var str in Body.ToString().Split('\n'))
-            {
-                sb.AppendLine("\t" + str);
-            }
-
-            sb.AppendLine("}");
-
-            return sb.ToString();
-        }
-
-        private string ParamsToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            if (_params.Count == 0)
-                return string.Empty;
+		public MethodBuilder(string accessModifier, bool isStatic, string returnType, string name, IEnumerable<(string type, string paramName)> @params = null)
+		{
+			_accessModifier = accessModifier;
+			_isStatic = isStatic;
+			_type = returnType;
+			_name = name;
+			if (@params is null)
+			{
+				_params = new List<(string type, string paramName)>();
+			}
+			else
+			{
+				_params = @params.ToList();
+			}
 
 
-            for (int i = 0; i < _params.Count; i++)
-            {
-                if (i < _params.Count - 1)
-                    sb.Append($"{_params[i].type} {_params[i].paramName}, ");
-                else
-                    sb.Append($"{_params[i].type} {_params[i].paramName}");
-            }
+		}
+		public MethodBuilder(string accessModifier, bool isStatic, string returnType, string name, params (string type, string paramName)[] @params)
+		{
+			_accessModifier = accessModifier;
+			_isStatic = isStatic;
+			_type = returnType;
+			_name = name;
+			_params = @params.ToList();
+		}
+		public MethodBuilder(string accessModifier, bool isStatic, string returnType, string name, IMethodSymbol @params)
+		{
+			_accessModifier = accessModifier;
+			_isStatic = isStatic;
+			_type = returnType;
+			_name = name;
+			_params = new List<(string type, string paramName)>();
 
-            return sb.ToString();
-        }
-        public string GetParamsNameString()
-        {
+			string paramType, paramName;
+
+			foreach (var symbol in @params.Parameters)
+			{
+				paramType = symbol.Type.ToDisplayString();
+				paramName = symbol.Name;
+
+				_params.Add((paramType, paramName));
+			}
+
+		}
+		public MethodBuilder(string returnType, string name, IEnumerable<(string type, string paramName)> @params = null, string accessModifier = "private", bool isStatic = false)
+		{
+			_accessModifier = accessModifier;
+			_isStatic = isStatic;
+			_type = returnType;
+			_name = name;
+			_params = @params.ToList();
+		}
+		public void AddParam(string paramType, string paramName, string defValue = null)
+		{
+			if (!(defValue is null))
+			{
+				paramName += $" = {defValue}";
+				_params.Insert(_params.Count, (paramType, paramName));
+			}
+			else
+			{
+				_params.Add((paramType, paramName));
+
+			}
+
+
+		}
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			string @static = _isStatic ? "static" : string.Empty;
+
+			sb.AppendLine($"{_accessModifier} {@static} {_type} {_name} ({ParamsToString()})");
+			sb.AppendLine("{");
+
+			foreach (var str in Body.ToString().Split('\n'))
+			{
+				sb.AppendLine("\t" + str);
+			}
+
+			sb.AppendLine("}");
+
+			return sb.ToString();
+		}
+
+		private string ParamsToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			if (_params.Count == 0)
+				return string.Empty;
+
+
+			for (int i = 0; i < _params.Count; i++)
+			{
+				if (i < _params.Count - 1)
+					sb.Append($"{_params[i].type} {_params[i].paramName}, ");
+				else
+					sb.Append($"{_params[i].type} {_params[i].paramName}");
+			}
+
+			return sb.ToString();
+		}
+		public string GetParamsNameString()
+		{
 			StringBuilder sb = new StringBuilder();
 			if (_params.Count == 0)
 				return string.Empty;
@@ -119,7 +141,7 @@ namespace DIFactoryGenerator.Builders
 
 			return sb.ToString();
 		}
-        public IEnumerable<(string paramType, string paramName)> EnumerateParams() => _params;
-        
-    }
+		public IEnumerable<(string paramType, string paramName)> EnumerateParams() => _params;
+
+	}
 }
